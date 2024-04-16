477b40fb3b3ea05a529dae66a5a4581ca8b29aae
Sky Martin <131005117+skylukemartin@users.noreply.github.com>
2024-04-16 19:17:37 +0200

    Reformat and rewrite UnitHelper comments
    *Reformat and rewrite comments according to required format

--- RecipeProject/Classes/UnitHelper.cs
+++ RecipeProject/Classes/UnitHelper.cs
@@ -1,9 +1,25 @@
+/// <summary>
+/// Name: Sky Martin
+/// Student: ST10286905
+/// Module: PROG6221
+/// References: https://learn.microsoft.com/en-us/dotnet/api/system.enum.getvalues?view=netframework-4.8
+///             https://learn.microsoft.com/en-us/dotnet/api/system.array.findlast?view=netframework-4.8
+///             https://www.bytehide.com/blog/enum-to-array-csharp
+/// </summary>
+
 using System;
 
 namespace RecipeProject.Classes
 {
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+    /// <summary>
+    /// This class contains an enum to represent different units of measurement,
+    /// as well as a method for finding the most appropriate unit of measurement
+    /// for a given number of milliliters.
+    /// </summary>
     public static class UnitHelper
     {
+        // Declare and define units of measurement enum.
         public enum Units
         {
             Milliliter = 1,
@@ -13,16 +29,19 @@ namespace RecipeProject.Classes
             Liter = 1000
         };
 
-        // Find and return the best unit for the given number of milliliters.
-        // Assuming the best unit of measurement is the one with the
-        // closest mlPerUnit below the given milliliters.
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+        /// <summary>
+        /// Find and return the most appropriate unit of measurement for the given number of milliliters.
+        /// Assumes best unit of measurement is unit with nearest mlPerUnit less than given milliliters.
+        /// </summary>
         public static Units FindBestUnit(float milliliters)
         {
-            // Sources: https://learn.microsoft.com/en-us/dotnet/api/system.enum.getvalues?view=netframework-4.8
-            //          https://learn.microsoft.com/en-us/dotnet/api/system.array.findlast?view=netframework-4.8
-            //          https://www.bytehide.com/blog/enum-to-array-csharp
+            // Create units array by getting values of units enum, then casting them to units array.
             Units[] units = (Units[])Enum.GetValues(typeof(Units));
-            return Array.FindLast(units, unit => (int)unit <= milliliters);
+            // Use Array.FindLast method to loop through units array backwards,
+            // return first unit where mlPerUnit threshold is below given milliliters
+            // (technically last matching element, but the function loops backwards).
+            return Array.FindLast(units, mlPerUnit => (int)mlPerUnit <= milliliters);
         }
     }
 }
